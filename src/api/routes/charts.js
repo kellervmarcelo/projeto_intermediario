8df33ad031fef3eb1b716141fd12049493683b89
Marcelo Keller <marcelokv123@gmail.com>
2021-05-27 05:27:37 -0300

    algumas mudancas

--- src/api/routes/charts.js
+++ src/api/routes/charts.js
@@ -3,29 +3,29 @@ const route = express.Router()
 const puppeteer = require('puppeteer')
 
 route.get("/", async function (req, res) {
-  const browser = await puppeteer.launch();
+  var query = req.query.consulta
+
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto("https://www.billboard.com/charts/hot-100");
 
   const list = await page.evaluate(() => {
-    // const nodelist = document.querySelectorAll("#top20 tr td:nth-child(4)");
-    // const nodelist2 = document.querySelectorAll("#top20 tr td:nth-child(5)");
-    // const linguagens = [...nodelist];
-    // const porcentagem = [...nodelist2];
-    // linguagens.forEach((e, i) => {
-    //   var c = {
-    //     linguagem: e.innerHTML,
-    //     porcentagem: porcentagem[i].innerHTML,
-    //   };
-    //   console.log(c)
-    // });
+    
+    const nodelist = document.querySelectorAll("#top20 tr td:nth-child(4)");
+    const nodelist2 = document.querySelectorAll("#top20 tr td:nth-child(5)");
+    const linguagens = [...nodelist];
+    const porcentagem = [...nodelist2];
+
+    
+    
     return {
+      'position': document.querySelector('.chart-element__rank__number').innerHTML,
       'song': document.querySelector(".chart-element__information__song").innerHTML,
       'artist': document.querySelector(".chart-element__information__artist").innerHTML,
       timestamp: Date.now(),
     };
   });
-  await browser.close();
+  
   console.log(list);
 
   res.json(list);
